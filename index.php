272c137bcdfd124e01c7ef8f98cba0e76f7ee2d3
Gabriele0600 <rtnprovini@gmail.com>
2025-05-02 22:42:26 +0200

    prima commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="it">
+    <head>
+        <meta charset="UTF-8">
+        <title>Sito Commerciale</title>
+        <link rel="stylesheet" type="text/css" href="">
+    </head>
+    <body>
+        <?php include 'header.html'; ?>
+        
+    </body>
+</html>
